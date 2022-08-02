c85749ce1f07ece9dbf765f15a655285e16fb9be
Precious <preciousmaregwa1997@gmail.com>
2022-08-02 10:27:12 +0300

    add plants controllers

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,11 @@
 class PlantsController < ApplicationController
+    def index
+        plants = Plant.all
+        render json: plants
+    end
+
+    def show
+        plant = Plant.find(params[:id])
+        render json: plant
+    end
 end
