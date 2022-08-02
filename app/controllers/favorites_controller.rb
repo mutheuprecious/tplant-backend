4a7f3a25ee9475f3a1bca3422d04a3a30c16b701
Precious <preciousmaregwa1997@gmail.com>
2022-08-02 10:20:17 +0300

    add favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,23 @@
 class FavoritesController < ApplicationController
+    def create
+        print params
+        if Plant.find(params[:favorite][:plant_id]) && User.find(params[:favorite][:user_id])
+            new_favorite = Favorite.create(favorite_params)
+        else 
+            new_favorite = favorite_params
+        end
+        render json: new_favorite.to_json(include: :plant, except:[:user_id, :plant_id, :created_at, :updated_at])
+    end
+
+    def destroy
+        favorite = Favorite.find(params[:id])
+        favorite.destroy
+        render json: favorite
+    end
+
+    private
+
+    def favorite_params
+        params.require(:favorite).permit(:plant_id, :user_id)
+    end
 end
