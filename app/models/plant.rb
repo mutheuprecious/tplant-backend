13959500a1e4e180d85b4e9cf873ad9f902d9253
Precious <preciousmaregwa1997@gmail.com>
2022-08-02 12:32:33 +0300

    add changes

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,2 +1,4 @@
 class Plant < ApplicationRecord
+    has_many :transactions
+    has_many :users, through: :transactions
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_secure_password
+    
+    has_many :transactions
+    has_many :plants, through: :transactions
+    has_many :favorites
+
+    validates :email, uniqueness: true 
 end
